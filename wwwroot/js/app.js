39528be02bef4f374a29910c5f2a47ebdde28bc4
Yufan Lou <2263580+louy2@users.noreply.github.com>
2024-07-20 03:32:39 +0800

    add persist name with localStorage

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,5 +1,15 @@
 async function main() {
   // main fn
+  const yourNameEl = document.querySelector('#your-name');
+  const yourNameOld = localStorage.getItem('your-name');
+  if (!yourNameOld) {
+    localStorage.setItem('your-name', yourNameEl.textContent);
+  } else {
+    yourNameEl.textContent = yourNameOld;
+  }
+  yourNameEl.addEventListener('input', () => {
+    localStorage.setItem('your-name', yourNameEl.textContent);
+  })
 }
 
 document.addEventListener('DOMContentLoaded', main);
