1e869f0e8bbec7bb19c384f38e3cd085cb585599
Yufan Lou <2263580+louy2@users.noreply.github.com>
2024-07-20 03:49:33 +0800

    misc

--- wwwroot/js/app.js
+++ wwwroot/js/app.js
@@ -1,5 +1,7 @@
 async function main() {
-  // main fn
+  // main fn is run once when DOM is loaded
+
+  // recover name from localStorage and show in #your-name
   const yourNameEl = document.querySelector('#your-name');
   const yourNameOld = localStorage.getItem('your-name');
   if (!yourNameOld) {
@@ -7,6 +9,7 @@ async function main() {
   } else {
     yourNameEl.textContent = yourNameOld;
   }
+  // save name in #your-name into localStorage
   yourNameEl.addEventListener('input', () => {
     localStorage.setItem('your-name', yourNameEl.textContent);
   })
